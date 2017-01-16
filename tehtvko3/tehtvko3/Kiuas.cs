fb43a329e8a4fde9a4426fb628eebd1497476e5d
reapercage <h4026@student.jamk.fi>
2017-01-16 13:45:59 +0200

    teht1 kiuas

---
+++ tehtvko3/tehtvko3/Kiuas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtvko3
+{
+    public class Kiuas
+    {
+        public bool Päällä { get; set; }
+        public double Lämpötila { get; set; }
+        //public double Kosteus { get; set; }
+        double kosteus;
+        //float kosteus;
+        public double Kosteus
+        {
+            get { return kosteus; }
+            set
+            {
+                kosteus = value;
+                if (kosteus < 0 || kosteus > 100)
+                {
+                    kosteus = 0;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ tehtvko3/tehtvko3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtvko3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KiukaanLämmitys();
+        }
+        static void KiukaanLämmitys()
+        {
+            Kiuas sampo = new tehtvko3.Kiuas();
+            sampo.Päällä = false;
+            sampo.Lämpötila = 0;
+            sampo.Kosteus = 4;
+            Console.Write("Laita kiuas päälle painamalla Enter");
+            Console.ReadKey();
+            sampo.Päällä = true;
+            Console.WriteLine();
+            if (sampo.Päällä == true)
+            {
+                Console.WriteLine("Kiuas on nyt päällä.");
+            }
+            Console.Write("Lisää lämpöä asteina > ");
+            double lisäälämpö = 0;
+            lisäälämpö = System.Convert.ToDouble(Console.ReadLine());
+            sampo.Lämpötila = sampo.Lämpötila + lisäälämpö;
+            Console.WriteLine("Kiukaan lämpötila on nyt "
+                                + sampo.Lämpötila + " astetta ja ilmankosteus "
+                                + sampo.Kosteus + " prosenttia.");
+            Console.WriteLine("Lorota vettä kiukaalle > ");
+            sampo.Kosteus = sampo.Kosteus + System.Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kiukaan lämpötila on nyt "
+                                + sampo.Lämpötila + " astetta ja ilmankosteus "
+                                + sampo.Kosteus + " prosenttia.");
+            Console.ReadKey();
+
+
+        }
+    }
+
+}

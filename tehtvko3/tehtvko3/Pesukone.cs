ad959aa61eeb713d2779689cf069f98e3e5c01ae
reapercage <h4026@student.jamk.fi>
2017-01-17 10:34:27 +0200

    Pesukone new

---
+++ tehtvko3/tehtvko3/Pesukone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtvko3
+{
+    class Pesukone
+    {
+        public bool Päällä { get; set; }
+        public int Kesto { get; set; }
+        public int Lämpö { get; set; }
+        public bool Linkous { get; set; }
+        int kierrokset;
+
+        //pesuohjelma
+        public int LinkousKierrokset
+        {
+            get { return kierrokset; }
+            set
+            {
+                kierrokset = value;
+                if (kierrokset < 0 || kierrokset > 2000)
+                {
+                    kierrokset = 0;
+                }
+            }
+
+        }
+    }
+}
